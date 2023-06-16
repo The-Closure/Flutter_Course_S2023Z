988c73c334a96ab3d5642607ac5e394bbce713db
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-06-16 11:54:05 -0700

    fix get it

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plan_and_go/data/local_data/local_storage.dart';
 
 import 'package:plan_and_go/view/component/textfield.dart';
 import 'package:plan_and_go/view/res/string.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'bloc/home_bloc.dart';
 
@@ -38,57 +40,54 @@ class MyHomePage extends StatelessWidget {
     TextEditingController username = TextEditingController();
 
     TextEditingController password = TextEditingController();
-    return BlocProvider(
-      create: (context) => HomeBloc(),
-      child: Builder(builder: (context) {
-        return Scaffold(
-            body: Center(
-          child: Container(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  myTextField(username),
-                  myTextField(password),
-                  ElevatedButton(
-                      onPressed: () {
-                        context.read<HomeBloc>().add(Login(username.text));
-                     showDialog(context: context, builder: (context) => newpage(context),);
-                      },
-                      child: Text("LogIn"))
-                ]),
-            width: 400,
-            height: 400,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: LinearGradient(
-                  colors: [Colors.lightBlue, Colors.redAccent],
-                )),
-          ),
-        ));
-      }),
-    );
+    return Builder(builder: (context) {
+      return Scaffold(
+          body: Center(
+        child: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                myTextField(username),
+                myTextField(password),
+                ElevatedButton(
+                    onPressed: () {
+                      myMagic
+                          .get<SharedPreferences>()
+                          .setString('username', username.text);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SecondPage(),
+                          ));
+                    },
+                    child: Text("LogIn"))
+              ]),
+          width: 400,
+          height: 400,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                colors: [Colors.lightBlue, Colors.redAccent],
+              )),
+        ),
+      ));
+    });
   }
 }
 
-  dynamic newpage(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        return Scaffold(
-          appBar: AppBar(),
-          body: Center(child: BlocBuilder<HomeBloc, HomeState>(
-            builder: (context, state) {
-              if (state is Success) {
-                return Text(
-                  "Hello Master ",
-                  style: TextStyle(fontSize: 32),
-                );
-              } else 
-                return CircularProgressIndicator();
-              
-            },
-          )),
-        );
-      }
-    );
-  
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Text(
+            "Hello Master ${myMagic.get<SharedPreferences>().getString('username')} ",
+            style: TextStyle(fontSize: 32),
+          ),
+        ),
+      );
+    });
+  }
 }
